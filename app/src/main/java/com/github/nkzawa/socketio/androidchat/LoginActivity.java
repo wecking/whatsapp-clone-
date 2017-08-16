4d2cc98210d3280d0aacf374d795f4dcd1b6df3a
wecking <wecking1@gmail.com>
2017-08-16 22:24:35 +0100

    implements chating and listing chats

---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/LoginActivity.java
@@ -0,0 +1,166 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * A login screen that offers login via username.
+ */
+public class LoginActivity extends Activity {
+
+    private EditText mUsernameView;
+
+    public static String mUsername = "599447910ca6190004d52d8d";
+
+    public  static String nickname;
+
+    private EditText mPhoneView;
+
+    private Socket mSocket;
+    private EditText mFullName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ChatApplication app = (ChatApplication) getApplication();
+        mSocket = app.getSocket();
+
+        // Set up the login form.
+        mUsernameView = (EditText) findViewById(R.id.username_input);
+         mPhoneView = (EditText) findViewById(R.id.Phone_input);
+        mFullName = (EditText) findViewById(R.id.FullName_input);
+        mUsernameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mSocket = app.getSocket();
+
+
+        mSocket.on("createUser", onLogin);
+        mSocket.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mSocket.off("createUser", onLogin);
+    }
+
+    /**
+     * Attempts to sign in the account specified by the login form.
+     * If there are form errors (invalid username, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors.
+        mUsernameView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString().trim();
+        String phone = mPhoneView.getText().toString().trim();
+        String fullName = mFullName.getText().toString().trim();
+
+        Log.d("king", username);
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            mUsernameView.setError(getString(R.string.error_field_required));
+            mUsernameView.requestFocus();
+            return;
+        }
+
+//        mUsername = username;
+        nickname = username;
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("fullname", fullName);
+            data.put("shortname", username);
+            data.put("phone", phone);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // perform the user login attempt.
+        mSocket.emit("createUser", data);
+    }
+
+    private Emitter.Listener onLogin = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = (JSONObject) args[0];
+//
+            Log.d("king", data.toString()+"hhhhhhh");
+//
+//            try {
+//                LoginActivity.mUsername = data.getString("messageby");
+//                Log.v("king", data.getString("messageby"));
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            int numUsers;
+//            try {
+//                numUsers = data.getInt("numUsers");
+//            } catch (JSONException e) {
+//                return;
+//            }
+
+            try {
+                Log.v("king",new JSONObject(new JSONObject(data.getString("message")).getString("info")).getString("_id").toString() + "kkkkk" );
+                mUsername = new JSONObject(new JSONObject(data.getString("message")).getString("info")).getString("_id");
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            LoginActivity.this.startActivity(intent);
+            LoginActivity.this.finish();
+//            Intent intent = new Intent();
+//            intent.putExtra("username", mUsername);
+            //            intent.putExtra("numUsers", numUsers);
+//            setResult(RESULT_OK, intent);
+//            finish();
+        }
+    };
+}
+
+
+
